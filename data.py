fe42f1f658604c7ebfccb23237f484546ae7eb10
AndryLev <bostonandry20@gmail.com>
2023-11-02 22:22:15 +0200

    Lab 3 , independent work

---
+++ data.py
@@ -0,0 +1,20 @@
+posts = [
+    {
+        'title': 'CSS and HTML JS',
+        'body': 'Worked with WEB',
+        'exp': '2 year',
+        'date_ms': 1593607500000,
+    },
+    {
+        'title': 'SQL and MY SQL',
+        'body': 'Worked with DB',
+        'exp': '1 year',
+        'date_ms': 1593870300000,
+    },
+    {
+        'title': 'Angular and React',
+        'body': 'Worked with this tools',
+        'exp': '5 month',
+        'date_ms': 1594419000000,
+    }
+]
