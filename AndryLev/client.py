95bf4b77f30ee0087af36e10596cf67e32f8b806
AndryLev <149687150+AndryLev@users.noreply.github.com>
2023-11-07 19:30:29 +0200

    Lab 1 and Lab 2

---
+++ AndryLev/client.py
@@ -0,0 +1,22 @@
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_address = ('127.0.0.1', 12345)
+client_socket.connect(server_address)
+
+while True:
+    message = input("Введіть текст для відправки або 'exit' для виходу: ")
+    if message.lower() == 'exit':
+        break
+
+    client_socket.send(message.encode('utf-8'))
+    response = client_socket.recv(1024).decode('utf-8')
+
+    if response == "Дані успішно відправлено":
+        print("Дані успішно відправлені")
+
+    if message.lower() == 'exit':
+        break
+
+client_socket.close()
